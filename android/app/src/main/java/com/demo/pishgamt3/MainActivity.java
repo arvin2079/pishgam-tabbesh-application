276c86ff0a41622145f919b562353859d5b72dcb
MASON <aliasmdev@gmail.com>
2020-04-30 23:06:35 +0430

    CreateBranchSingUp

--- android/app/src/main/java/com/demo/pishgamt3/MainActivity.java
+++ android/app/src/main/java/com/demo/pishgamt3/MainActivity.java
@@ -8,6 +8,8 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+
+
     GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
 }
