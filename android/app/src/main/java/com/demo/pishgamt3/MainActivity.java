02beccb537b8f9349c0e3e36231c6b2abb1c024c
MASON <aliasmdev@gmail.com>
2020-05-11 01:27:10 +0430

    Write commits

--- android/app/src/main/java/com/demo/pishgamt3/MainActivity.java
+++ android/app/src/main/java/com/demo/pishgamt3/MainActivity.java
@@ -10,21 +10,27 @@ public class MainActivity extends FlutterActivity {
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
+//Send input parameters to the server to enter the program
+
 
   public void SignIn()
   {
     OkHttpClient client=new OkHttpClient();
     String url="https://tabeshma.000webhostapp.com/mysites/showparams.php";
+    //create post method
     RequestBody formBody = new FormBody.Builder()
             .add("email", "eve.holt@reqres.in")
             .add("password","pistol")
             .build();
-
+  //send request parameter to server
     Request request = new Request.Builder()
             .url(url)
             .post(formBody)
             .build();
 
+//    Receive server parameters for encryption
+
+ //   Information is encrypted in JWTUtils
     client.newCall(request).enqueue(new Callback() {
       @Override
       public void onFailure(Call call, IOException e) {
