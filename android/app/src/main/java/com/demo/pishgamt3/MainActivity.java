980216340a78a1a314d5df904974f139f08f4412
MASON <aliasmdev@gmail.com>
2020-05-11 01:21:21 +0430

    Complete Signin

--- android/app/src/main/java/com/demo/pishgamt3/MainActivity.java
+++ android/app/src/main/java/com/demo/pishgamt3/MainActivity.java
@@ -10,4 +10,43 @@ public class MainActivity extends FlutterActivity {
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
+
+  public void SignIn()
+  {
+    OkHttpClient client=new OkHttpClient();
+    String url="https://tabeshma.000webhostapp.com/mysites/showparams.php";
+    RequestBody formBody = new FormBody.Builder()
+            .add("email", "eve.holt@reqres.in")
+            .add("password","pistol")
+            .build();
+
+    Request request = new Request.Builder()
+            .url(url)
+            .post(formBody)
+            .build();
+
+    client.newCall(request).enqueue(new Callback() {
+      @Override
+      public void onFailure(Call call, IOException e) {
+        Log.i("4444",e.getMessage());
+      }
+
+      @Override
+      public void onResponse(Call call, Response response) throws IOException
+      {
+        if(response.isSuccessful())
+        {
+          final String myresponse=response.body().string();
+          Main2Activity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+          });
+        }
+
+      }
+    });
+  }
+
 }
