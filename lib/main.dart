fdc308ed82a22a781ac602e353ba44e7a71bb538
arvin sadeghi <arving2079@gmail.com>
2020-05-13 22:14:35 +0430

    cleaning code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pishgamv2/screens/signup_page.dart';
 import 'package:pishgamv2/screens/singin_page.dart';
-import 'package:pishgamv2/screens/splash_screen.dart';
 import 'brain/authBloc.dart';
 import 'constants/PishgamTheme.dart';
 
@@ -18,7 +18,7 @@ class PishgamDemo extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: PTheme,
-        home: SigninPage(),
+        home: SignUpPage(),
       ),
     );
   }
--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -44,146 +44,144 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     double removeHeight = kToolbarHeight + MediaQuery.of(context).padding.top;
-    return SafeArea(
-      child: Scaffold(
-        //  resizeToAvoidBottomInset: false,
-        backgroundColor: Color(0xFF424242),
-        appBar: AppBar(
-          elevation: 0,
-          title: Text('Page Title'),
-          leading: IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {},
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFF424242),
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Page Title'),
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {},
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height - (removeHeight),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.fill,
           ),
         ),
-        body: SingleChildScrollView(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
           child: Container(
-            height: MediaQuery.of(context).size.height - (removeHeight),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/laptopDevicesWoodenDesk.png'),
-                fit: BoxFit.fill,
-              ),
-            ),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
-              child: Container(
-                color: Colors.white70.withOpacity(0.2),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30, top: 15),
-                      child: RichText(
-                        textDirection: TextDirection.rtl,
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'پیشگامی ',
-                              style: TextStyle(
-                                fontFamily: 'vazir',
-                                fontSize: 27,
-                                fontWeight: FontWeight.w100,
-                                color: Colors.white,
-                              ),
-                            ),
-                            TextSpan(
-                              text: 'شو',
-                              style: TextStyle(
-                                fontFamily: 'vazir',
-                                fontSize: 27,
-                                fontWeight: FontWeight.w100,
-                                color: Colors.limeAccent[700],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(30),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        shape: BoxShape.rectangle,
-                        color: Colors.black26,
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          SignupTextInput(
-                            labelText: 'نام',
-                            focusNode: _nameFocusNode,
-                            controller: _nameController,
-                            textInputType: TextInputType.text,
-                            onEditingComplete: _onNameEditingComplete,
-                          ),
-                          SignupTextInput(
-                            labelText: 'نام خانوادگی',
-                            focusNode: _familyNameFocusNode,
-                            controller: _familyNameController,
-                            textInputType: TextInputType.text,
-                            onEditingComplete: _onFamilyNameEditingComplete,
-                          ),
-                          SignupTextInput(
-                            labelText: 'نام کاربری',
-                            focusNode: _userNameFocusNode,
-                            controller: _userNameController,
-                            textInputType: TextInputType.text,
-                            onEditingComplete: _onUserNameEditingComplete,
-                          ),
-                          SignupTextInput(
-                            labelText: 'رمز عبور',
-                            focusNode: _passwordFocusNode,
-                            controller: _passwordController,
-                            textInputType: TextInputType.text,
-                            obscureText: true,
-                          ),
-                          SizedBox(height: 40),
-                          Row(
-                            textDirection: TextDirection.rtl,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              CustomDropDownButton(
-                                hint: 'مقطع',
-                                items: location,
-                              ),
-                              CustomDropDownButton(
-                                hint: 'شهر',
-                                items: location,
-                              ),
-                            ],
-                          ),
-                          RadioButton(
-                            txt: 'جنسيت',
-                            valueFirst: 1,
-                            valueSecond: 2,
-                            first: 'پسر',
-                            second: 'دختر',
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            child: FlatButton(
-                              color: Colors.yellowAccent[700],
-                              child: Text(
-                                'ثبت',
-                                style: TextStyle(
-                                  fontFamily: 'vazir',
-                                  fontWeight: FontWeight.w900,
-                                  fontSize: 17,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                              onPressed: () {},
+            color: Colors.white12.withOpacity(0.1),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, top: 15),
+                    child: RichText(
+                      textDirection: TextDirection.rtl,
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'پیشگامی ',
+                            style: TextStyle(
+                              fontFamily: 'vazir',
+                              fontSize: 27,
+                              fontWeight: FontWeight.w100,
+                              color: Colors.white,
                             ),
                           ),
-                          SizedBox(height: 35),
+                          TextSpan(
+                            text: 'شو',
+                            style: TextStyle(
+                              fontFamily: 'vazir',
+                              fontSize: 27,
+                              fontWeight: FontWeight.w100,
+                              color: Colors.limeAccent[700],
+                            ),
+                          ),
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      shape: BoxShape.rectangle,
+                      color: Colors.black26,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SignupTextInput(
+                          labelText: 'نام',
+                          focusNode: _nameFocusNode,
+                          controller: _nameController,
+                          textInputType: TextInputType.text,
+                          onEditingComplete: _onNameEditingComplete,
+                        ),
+                        SignupTextInput(
+                          labelText: 'نام خانوادگی',
+                          focusNode: _familyNameFocusNode,
+                          controller: _familyNameController,
+                          textInputType: TextInputType.text,
+                          onEditingComplete: _onFamilyNameEditingComplete,
+                        ),
+                        SignupTextInput(
+                          labelText: 'نام کاربری',
+                          focusNode: _userNameFocusNode,
+                          controller: _userNameController,
+                          textInputType: TextInputType.text,
+                          onEditingComplete: _onUserNameEditingComplete,
+                        ),
+                        SignupTextInput(
+                          labelText: 'رمز عبور',
+                          focusNode: _passwordFocusNode,
+                          controller: _passwordController,
+                          textInputType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 40),
+                        Row(
+                          textDirection: TextDirection.rtl,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            CustomDropDownButton(
+                              hint: 'مقطع',
+                              items: location,
+                            ),
+                            CustomDropDownButton(
+                              hint: 'شهر',
+                              items: location,
+                            ),
+                          ],
+                        ),
+                        RadioButton(
+                          txt: 'جنسيت',
+                          valueFirst: 1,
+                          valueSecond: 2,
+                          first: 'پسر',
+                          second: 'دختر',
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: FlatButton(
+                            color: Colors.yellowAccent[700],
+                            child: Text(
+                              'ثبت',
+                              style: TextStyle(
+                                fontFamily: 'vazir',
+                                fontWeight: FontWeight.w900,
+                                fontSize: 17,
+                                color: Colors.black87,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        SizedBox(height: 35),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
