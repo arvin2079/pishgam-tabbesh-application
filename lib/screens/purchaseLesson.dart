51e8bde7de1c03f295abefb4ab723c0e75e7c4c4
z-jyp <zahramazhar2000@gmail.com>
2020-05-15 14:01:02 +0430

    add body to PurchaseLesson

--- lib/screens/purchaseLesson.dart
+++ lib/screens/purchaseLesson.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pishgamv2/components/lessonsList.dart';
+import 'package:pishgamv2/components/purchaseLessonsCard.dart';
 
 class PurchaseLesson extends StatefulWidget {
   @override
@@ -10,6 +12,67 @@ class _PurchaseLessonState extends State<PurchaseLesson> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            LessonList(
+              title: 'رياضی',
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: <Widget>[
+                    PurchaseLessonCard(
+                      courseName: 'شیمی دهم',
+                      grade: 'پایه دهم',
+                      explanation:
+                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                      imageURL: 'assets/images/lessons.jpg',
+                    ),
+                    PurchaseLessonCard(
+                      courseName: 'شیمی دهم',
+                      grade: 'پایه دهم',
+                      explanation:
+                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                      imageURL: 'assets/images/lessons.jpg',
+                    ),
+                    PurchaseLessonCard(
+                      courseName: 'شیمی دهم',
+                      grade: 'پایه دهم',
+                      explanation:
+                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                      imageURL: 'assets/images/lessons.jpg',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            LessonList(
+              title: 'فیزیک',
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: <Widget>[
+                    PurchaseLessonCard(
+                      courseName: 'شیمی دهم',
+                      grade: 'پایه دهم',
+                      explanation:
+                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                      imageURL: 'assets/images/lessons.jpg',
+                    ),
+                    PurchaseLessonCard(
+                      courseName: 'شیمی دهم',
+                      grade: 'پایه دهم',
+                      explanation:
+                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                      imageURL: 'assets/images/lessons.jpg',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
