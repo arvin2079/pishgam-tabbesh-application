b240b4a6521e285eb675d988c874e55241b990a2
z-jyp <zahramazhar2000@gmail.com>
2020-05-15 13:30:46 +0430

    create class PurchaseLesson

---
+++ lib/screens/purchaseLesson.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PurchaseLesson extends StatefulWidget {
+  @override
+  _PurchaseLessonState createState() => _PurchaseLessonState();
+}
+
+class _PurchaseLessonState extends State<PurchaseLesson> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+    );
+  }
+}
