87dd17a713cacc1c1925fd005dee8ccfae4e6611
z-jyp <zahramazhar2000@gmail.com>
2020-05-15 18:33:49 +0430

    add appBar to PurchaseLesson

--- lib/screens/purchaseLesson.dart
+++ lib/screens/purchaseLesson.dart
@@ -8,69 +8,126 @@ class PurchaseLesson extends StatefulWidget {
 }
 
 class _PurchaseLessonState extends State<PurchaseLesson> {
+  int counter = 4;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            LessonList(
-              title: 'رياضی',
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: <Widget>[
-                    PurchaseLessonCard(
-                      courseName: 'شیمی دهم',
-                      grade: 'پایه دهم',
-                      explanation:
-                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
-                      imageURL: 'assets/images/lessons.jpg',
-                    ),
-                    PurchaseLessonCard(
-                      courseName: 'شیمی دهم',
-                      grade: 'پایه دهم',
-                      explanation:
-                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
-                      imageURL: 'assets/images/lessons.jpg',
-                    ),
-                    PurchaseLessonCard(
-                      courseName: 'شیمی دهم',
-                      grade: 'پایه دهم',
-                      explanation:
-                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
-                      imageURL: 'assets/images/lessons.jpg',
-                    ),
-                  ],
-                ),
-              ),
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[300],
+          elevation: 0,
+          title: Text(
+            'خريد درس',
+            style: TextStyle(
+              fontFamily: 'vazir',
+              fontWeight: FontWeight.w100,
+              fontSize: 25,
+              color: Colors.black,
             ),
-            LessonList(
-              title: 'فیزیک',
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: <Widget>[
-                    PurchaseLessonCard(
-                      courseName: 'شیمی دهم',
-                      grade: 'پایه دهم',
-                      explanation:
-                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
-                      imageURL: 'assets/images/lessons.jpg',
-                    ),
-                    PurchaseLessonCard(
-                      courseName: 'شیمی دهم',
-                      grade: 'پایه دهم',
-                      explanation:
-                          'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
-                      imageURL: 'assets/images/lessons.jpg',
-                    ),
-                  ],
+          ),
+          actions: <Widget>[
+            Stack(
+              children: <Widget>[
+                IconButton(
+                  iconSize: 25,
+                  onPressed: () {},
+                  icon: Icon(Icons.shopping_cart),
+                  color: Colors.black,
                 ),
-              ),
+                Positioned(
+                  left: 5,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(30)),
+                    constraints: BoxConstraints(
+                      minWidth: 17,
+                      minHeight: 15,
+                    ),
+                    child: Text(
+                      counter.toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13,
+                        fontWeight: FontWeight.w100,
+                        fontFamily: 'vazir',
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            IconButton(
+              iconSize: 25,
+              onPressed: () {},
+              icon: Icon(Icons.close),
+              color: Colors.black,
             ),
           ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              LessonList(
+                title: 'رياضی',
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: <Widget>[
+                      PurchaseLessonCard(
+                        courseName: 'شیمی دهم',
+                        grade: 'پایه دهم',
+                        explanation:
+                            'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                        imageURL: 'assets/images/lessons.jpg',
+                      ),
+                      PurchaseLessonCard(
+                        courseName: 'شیمی دهم',
+                        grade: 'پایه دهم',
+                        explanation:
+                            'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                        imageURL: 'assets/images/lessons.jpg',
+                      ),
+                      PurchaseLessonCard(
+                        courseName: 'شیمی دهم',
+                        grade: 'پایه دهم',
+                        explanation:
+                            'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                        imageURL: 'assets/images/lessons.jpg',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              LessonList(
+                title: 'فیزیک',
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: <Widget>[
+                      PurchaseLessonCard(
+                        courseName: 'شیمی دهم',
+                        grade: 'پایه دهم',
+                        explanation:
+                            'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                        imageURL: 'assets/images/lessons.jpg',
+                      ),
+                      PurchaseLessonCard(
+                        courseName: 'شیمی دهم',
+                        grade: 'پایه دهم',
+                        explanation:
+                            'شیمی دهم با مهدی شهبازی دارنده مدال برنز المپیاد شیمی',
+                        imageURL: 'assets/images/lessons.jpg',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
