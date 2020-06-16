9ef91e797c9ea66451b30f147f4a6c0624503502
MASON <aliasmdev@gmail.com>
2020-06-16 14:02:18 +0430

    convert thread to handler

--- android/app/src/main/java/com/demo/pishgamt3/channel_result/MainThreadResult.java
+++ android/app/src/main/java/com/demo/pishgamt3/channel_result/MainThreadResult.java
@@ -1,36 +1,36 @@
 package com.demo.pishgamt3.channel_result;
 
+import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 
 import androidx.annotation.Nullable;
 
-import java.util.HashMap;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 import io.flutter.plugin.common.MethodChannel;
 
 public class MainThreadResult implements MethodChannel.Result {
 
     MethodChannel.Result result;
+    Handler handler;
 
 
-    public MainThreadResult(MethodChannel.Result result) {
+    public MainThreadResult(MethodChannel.Result result)
+    {
         this.result = result;
+        handler=new Handler(Looper.getMainLooper());
     }
 
     @Override
     public void success(@Nullable Object object)
     {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                result.success(object);
-            }
-        });
-
-
-
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        result.success(object);
+                    }
+                });
 
     }
 
@@ -38,26 +38,26 @@ public class MainThreadResult implements MethodChannel.Result {
     public void error(String errorCode, @Nullable String errorMessage, @Nullable Object errorDetails)
     {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                result.error(errorCode,errorMessage,errorDetails);
-
-            }
-        });
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        result.error(errorCode, errorMessage, errorDetails);
+                    }
+                });
 
     }
 
     @Override
     public void notImplemented() {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                result.notImplemented();
-
-            }
-        });
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        result.notImplemented();
+                    }
+                });
 
     }
 }
