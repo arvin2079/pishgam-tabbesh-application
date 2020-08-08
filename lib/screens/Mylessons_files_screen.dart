ad6ae42ef42ffa9d833ca063bbd209da1236a634
arvin sadeghi <arving2079@gmail.com>
2020-08-08 17:39:34 +0430

    my file Class structure created

---
+++ lib/screens/Mylessons_files_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class MylessonFiles extends StatelessWidget {
+  MyLessonFilesViewModel viewModel;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MyLessonFilesViewModel {
+
+}
